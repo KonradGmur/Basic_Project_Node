ab6b2524e3a3909dbbe55181e725d9f9b5478592
Konrad <konrad.gmur@gmail.com>
2020-09-29 09:38:39 -0700

    Create a simple server

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const utils = require("./utils");
+const http = require("http");
+const port = 3000;
 
-utils.greeting();
-utils.add(3, 6);
+const handler = (request, response) => {
+  console.log("New user!");
+};
+const server = http.createServer(handler);
+
+server.listen(port, () => {
+  console.log("server start");
+});
