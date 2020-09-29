d490d0cbcd784e054fabbbdb5a318e952de0dd01
Konrad <konrad.gmur@gmail.com>
2020-09-29 09:43:54 -0700

    Adding an express library to the project

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const port = 3000;
 
 const handler = (request, response) => {
   console.log("New user!");
+  response.end("Hello User");
 };
 const server = http.createServer(handler);
 
-server.listen(port, () => {
+server.listen(port, (err) => {
+  if (err) {
+    return console.log("Something was wrong");
+  }
   console.log("server start");
 });
