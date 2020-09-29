8bca2b02d5121b8a85342858b30b70a7526994f1
Konrad <konrad.gmur@gmail.com>
2020-09-29 10:39:17 -0700

    update app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const port = 3000;
 
+const genereteTitle = () => {
+  return "Node JS";
+};
+
 const app = express();
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  const title = genereteTitle();
+
+  res.render("index", {
+    pageTitle: title,
+    pageBody: "Hello Node",
+  });
 });
 
 app.get("/kontakt", (req, res) => {
